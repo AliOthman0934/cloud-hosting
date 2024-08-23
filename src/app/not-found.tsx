6ddefb94f55bc215709c73f138ba1abf9ca3ccd0
Ali Othman <alioth840@gmail.com>
2024-08-23 19:53:59 +0200

    Styling the custom not-found page with tailwind CSS

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,10 +4,10 @@ import Link from "next/link"
 
 const notFoundPage = () => {
     return (
-        <section>
-            <h1>404</h1>
-            <p>Page Not Found</p>
-            <Link href="/">
+        <section className="flex justify-center items-center flex-col min-h-screen">
+            <h1 className="text-7xl text-gray-800 font-bold ">404</h1>
+            <p className="text-gray-500 text-3xl mt-2 mb-5">Page Not Found</p>
+            <Link href="/" className="text-xl underline text-blue-700">
                 Go To Home 
             </Link>
         </section>
