0af3fa479d31b5e87da9ed4946e4655d5cb286d9
Ali Othman <alioth840@gmail.com>
2024-08-29 18:42:29 +0200

    Creating RegisterPage

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Register = () => {
     return (
-        <div> Register</div>
+        <section className='fix-height container px-7 flex items-center justify-center pt-16'>
+        <div className='m-auto bg-white rounded-lg p-5 w-2/3 md:h-2/3 '>
+            <h1 className='text-3xl font-bold text-gray-800 mb-5'>Create New Account</h1>
+        </div>
+    </section>
     )
 }
 
