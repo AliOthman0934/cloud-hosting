db91d4bf0554692f37a545e8683a3f5cfe09cd5f
Ali Othman <alioth840@gmail.com>
2024-09-02 22:02:18 +0200

    Adding form to add new article

---
+++ src/app/admin/AddArticleForm.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React, { ReactEventHandler, useState } from "react"
+import { toast } from "react-toastify";
+
+const AddArticleForm = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const submitFormHandler = (e:React.FormEvent) => {
+        e.preventDefault();
+        if(title === ""){
+            return toast.error("Title Is Required")
+        }
+        
+        if(description === ""){
+            return toast.error("Description Is Required")
+        }
+    }
+    return (
+        <form className='flex flex-col' onSubmit={submitFormHandler}>
+            <input
+                type='text'
+                placeholder='Inter Title'
+                value={title}
+                onChange={(e)=> setTitle(e.target.value)}
+                className='mb-4 border rounded p-2 text-xl' />
+                <textarea
+                rows={5}
+                placeholder="Inter Description"
+                value={description}
+                onChange={(e)=> setDescription(e.target.value)}
+                className="mb-4 p-2 lg:text-xl rounded resize-none"
+                />
+            <button type='submit' className='text-2xl text-white bg-blue-700 hover:bg-blue-900 p-2 rounded-lg font-bold'>Login</button>
+        </form>
+    )
+}
+
+export default AddArticleForm
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import AddArticleForm from './AddArticleForm'
 
 const AdminPage = () => {
     return (
-        <div>Admin</div>
+        <div className='h-[70vh] flex items-center justify-center px-5 lg:px-20'>
+            <div className='shadow p-4 bg-purple-200 rounded w-full'>
+                <h2 className='text-xl lg:text-2xl text-gray-700 font-semibold mb-4r'>Add New Article</h2>
+                <AddArticleForm/>
+            </div>
+        </div>
     )
 }
 
