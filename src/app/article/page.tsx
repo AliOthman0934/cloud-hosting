8e9aceb2e107bcf69498d84330dc34ef3810b5c2
Ali Othman <alioth840@gmail.com>
2024-09-07 08:39:49 +0200

    Adding loading spiner

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Article from "../components/Article/Article";
-import {typeArticles} from "@/utils/types"
-import { error } from "console";
+import { typeArticles } from "@/utils/types"
 import SearchArticle from "../components/Article/SearchArticle";
 import Pagination from "../components/Article/pagination";
 
@@ -9,19 +8,19 @@ import Pagination from "../components/Article/pagination";
 const Articlepage = async () => {
     const getArticles = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-    if(!getArticles.ok){
+    if (!getArticles.ok) {
         throw new Error("Somthing went wrong try agin")
     }
     const articles: typeArticles[] = await getArticles.json();
     return (
         <section className="container m-auto px-5 h-screen">
-            <SearchArticle/>
+            <SearchArticle />
             <div className="flex justify-center items-center flex-wrap gap-7">
-                {articles.slice(0,6).map(item =>
-                    <Article article={item} key={item.id}/>
+                {articles.slice(0, 6).map(item =>
+                    <Article article={item} key={item.id} />
                 )}
             </div>
-            <Pagination/>
+            <Pagination />
         </section>
 
     )
