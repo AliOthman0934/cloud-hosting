12859784618e033e04793ba9203282b26826d3d6
Ali Othman <alioth840@gmail.com>
2024-09-11 11:11:59 +0200

    POST Route Handler

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { articlesData } from "../../../utils/data"
+import { typeArticles } from "@/utils/types";
 
 
 /**
@@ -13,8 +14,25 @@ import { articlesData } from "../../../utils/data"
     - Imported articles data from the utils/data file for the response.
  */
 
-
-
 export function GET(request: NextRequest) {
     return NextResponse.json(articlesData, { status: 200 })
+}
+
+
+interface createArticle {
+    body: string,
+    title: string
+}
+
+export async function POST(request: NextRequest) {
+    const body = await (request.json()) as createArticle;
+    console.log(body);
+    const newArticle: typeArticles = {
+        title: body.title,
+        body: body.body,
+        id: articlesData.length + 1,
+        userId: 202
+    }
+    articlesData.push(newArticle)
+    return NextResponse.json(newArticle, { status: 201 })
 }
