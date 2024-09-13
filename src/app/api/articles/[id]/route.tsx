1a7e192cb7677de958eb109c91adf7e99e7d7223
Ali Othman <alioth840@gmail.com>
2024-09-13 22:47:56 +0200

    Created PUT handler for update single article

--- src/app/api/articles/[id]/route.tsx
+++ src/app/api/articles/[id]/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest,NextResponse } from "next/server";
 import {articlesData} from "@/utils/data"
+import {updateArticle} from "@/utils/postType"
 
 
 
@@ -25,3 +26,29 @@ export function GET(request:NextRequest, {params}:articleId){
     }
     return NextResponse.json({singleArticle},{status:200});
 }
+
+
+/**
+ * @method PUT 
+ * @route http://localhost:3000/api/articles/:id
+ * @access public
+ * @description 
+ *  - Created PUT handler for update single article data using Next.js API routes.
+    - Utilized `NextRequest` and `NextResponse` from "next/server" to return the data.
+    - Added a status code of 200 for successful responses.
+    - Imported articles data from the utils/data file for the response.
+ */
+
+    interface articleId{
+        params: {id:string}
+    }
+    
+    export async function PUT(request:NextRequest, {params}:articleId){
+        const singleArticle = articlesData.find(a => a.id === parseInt(params.id));
+        if(!singleArticle){
+            return NextResponse.json({message:"Article Not Found"},{status:404})
+        }
+        const body = (await request.json()) as updateArticle ;
+        console.log(body)
+        return NextResponse.json({message:"Article updated"},{status:200});
+    }
