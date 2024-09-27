2fa751ac061538868894c7fe6203e6c53ef9dab6
Ali Othman <alioth840@gmail.com>
2024-09-27 08:17:42 +0200

    Add verifyToken utility function for JWT verification in utils

---
+++ src/utils/verifyToken.ts
@@ -0,0 +1,24 @@
+import jsonwebtoken from "jsonwebtoken";
+import { NextRequest } from "next/server";
+import { typeJwt } from "@/utils/types";
+
+export function verifyToken(request: NextRequest): typeJwt | null {
+    try {
+        // Retrieve the token from cookies
+        const cookieToken = request.cookies.get("cookieToken");
+        const token = cookieToken?.value as string;
+
+        if (!token) return null;
+
+        // Get the secret key for verifying the JWT
+        const secretKey = process.env.JWT_KEY as string;
+
+        // Verify the token and cast it to `unknown` first to safely cast it to `typeJwt`
+        const payload = jsonwebtoken.verify(token, secretKey) as unknown as typeJwt;
+
+        return payload;  // Return the decoded payload
+    } catch (error) {
+        console.error("Error Verifying The Token. Try again later:", error);  // Log the actual error
+        return null;  // Return null on error or token verification failure
+    }
+}
