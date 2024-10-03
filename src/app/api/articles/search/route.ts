1754c3f20b2dd0a7e90528f834ce17a888aac59d
Ali Othman <alioth840@gmail.com>
2024-10-03 17:15:55 +0200

    Add documentation for GET /api/articles/search? endpoint search bar

--- src/app/api/articles/search/route.ts
+++ src/app/api/articles/search/route.ts
@@ -1,6 +1,17 @@
 import prisma from "@/utils/db";
 import { NextRequest,NextResponse } from "next/server";
 
+/**
+ * @method GET 
+ * @route http://localhost:3000/api/articles/search?searchText=Java
+ * @access public
+ * @description 
+ *  - Implemented search functionality in the GET handler to fetch articles based on a search query.
+    - If `searchText` is provided in the request parameters, articles are filtered by their title.
+    - If no search query is provided, a default set of 6 articles is returned.
+    - Added error handling for potential server issues during article fetching.
+    - This feature allows users to search for articles by title through a search bar, improving user experience and discoverability.
+ */
 
 export async function GET(request:NextRequest){
     try {
