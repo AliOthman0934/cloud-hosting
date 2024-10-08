2a3d2da6b3c1cba5f3996e627414f5eca4414737
Ali Othman <alioth840@gmail.com>
2024-10-08 19:13:56 +0200

    Pagination functionality

--- src/app/components/Article/pagination.tsx
+++ src/app/components/Article/pagination.tsx
@@ -1,12 +1,18 @@
-const paginationArray = [1, 2, 3, 4, 5];
+interface paginationProps {
+    page : number,
+    pageNumber : number,
+    route : string
+}
 
-const Pagination = () => {
+const Pagination = ({page, pageNumber,route} : paginationProps) => {
+    let pageArray:number[] = [];
+    for(let i = 1; i<=page ; i++ ) pageArray.push(i)
     return (
         <div className="flex justify-center items-center space-x-2 m-auto my-5">
             <div className="p-2 border rounded cursor-pointer hover:bg-gray-200">
                 Prev
             </div>
-            {paginationArray.map(page => (
+            {pageArray.map(page => (
                 <div key={page} className="p-2 border rounded cursor-pointer hover:bg-gray-200">
                     {page}
                 </div>
