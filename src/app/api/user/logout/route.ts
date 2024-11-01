f01710f69040f10ae211c575dce732cc363e7d14
Ali Othman <alioth840@gmail.com>
2024-11-01 10:36:44 +0100

    Resolve 405 Method Not Allowed error on logout

--- src/app/api/user/logout/route.ts
+++ src/app/api/user/logout/route.ts
@@ -1,12 +1,26 @@
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function GET(request: NextRequest) {
+//     try {
+//         cookies().delete("cookieToken")
+//         return NextResponse.json({ message: "Log Out" }, { status: 200 })
+//     } catch (error) {
+//         return NextResponse.json({ message: "Internal server error" }, { status: 500 });
+//     }
+// }
+
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
     try {
-        cookies().delete("cookieToken")
-        return NextResponse.json({ message: "Log Out" }, { status: 200 })
+        const response = NextResponse.json({ message: "Log Out" }, { status: 200 });
+        response.headers.set(
+            "Set-Cookie",
+            "cookieToken=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Strict"
+        );
+        return response;
     } catch (error) {
         return NextResponse.json({ message: "Internal server error" }, { status: 500 });
     }
 }
-
