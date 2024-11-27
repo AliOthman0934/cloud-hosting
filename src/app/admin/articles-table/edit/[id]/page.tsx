db6dabd1f0c9215269734efa2e9a63dfd51d120d
Ali Othman <alioth840@gmail.com>
2024-11-27 11:14:38 +0100

    add EditArticlePage to admin dashboard

---
+++ src/app/admin/articles-table/edit/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getSingleArticle } from '@/apiCalles/getArticlesApi'
+import { Article } from '@prisma/client'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+interface EditArticlePageProps {
+    params : {id:string}
+}
+
+const EditArticlePage = async ({params}:EditArticlePageProps) => {
+    const token = cookies().get("cookieToken")?.value || ""
+    if(!token) redirect("/")
+
+    const article: Article = await getSingleArticle(params.id)
+    return (
+        <section className='fix-height flex items-center justify-center px-5 lg:px-20'>
+            <div className='shadow p-4 bg-purple-200 rounded w-full'>
+                <h2 className='text-2xl text-green-700 font-semibold mb-4'>
+                    Edit Article
+                </h2>
+                    
+            </div>
+        </section>
+    )
+}
+
+export default EditArticlePage
